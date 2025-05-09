b3a1b1b4ae243783f5596bbd39f3974030661bdd
kuroi042 <mbouderr@Kenshin>
2025-05-09 19:32:14 +0100

    Add get product by id

--- crudapi/apis/urls.py
+++ crudapi/apis/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns=[
     path('products', views.Product_list , name='products'),
+path('product/<int:pk>/' , views.product , name='product'),
 ]
--- crudapi/apis/views.py
+++ crudapi/apis/views.py
@@ -4,18 +4,37 @@ from rest_framework.response import Response
 from . models import Product
 from . serializers import ProductSerializer
 from rest_framework.decorators import api_view
-
+from rest_framework import status
 
 @api_view(['GET' , 'POST'])
 def Product_list(request):
     if request.method == 'GET':
+        #fetch all databases
         products = Product.objects.all()
+        #many=True for serializing all products
         serializer =  ProductSerializer(products ,many=True)
+        #return json file serializer
         return Response(serializer.data)
     
     if request.method =='POST':
-        serializer = ProductSerializer(data =request.data) # get the data from the request
+        # get the data from the request
+        serializer = ProductSerializer(data =request.data) 
+        #check if the data is valid
         if serializer.is_valid():
+            #save the product in the db
             serializer.save()
+            #return the new created product 
             return Response(serializer.data)
-        
+
+
+@api_view(['GET' , 'PUT' , 'DELETE'])
+def product(request, pk): # request referes to GET put delete
+    try:
+        #primarry key 
+        product =Product.objects.get(id=pk) #find the product using id
+    except Product.DoesNotExist(): 
+        return Response(status=404)
+# Productserializer convert the product Object to Jsonfile format 
+    if request.method == 'GET':
+        serializer =  ProductSerializer(product)
+        return Response(serializer.data)
